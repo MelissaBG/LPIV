96325ed73abfe701b71a66598cd429a5d7fbf1a3
Melissa Bernardes Goulart <91767583+MelissaBG@users.noreply.github.com>
2023-01-04 21:31:19 -0300

    Add files via upload

---
+++ LoginActivity/app/src/main/java/com/example/loginactivity/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.example.loginactivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val buttonClick = findViewById<Button>(R.id.button_clickHome)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
